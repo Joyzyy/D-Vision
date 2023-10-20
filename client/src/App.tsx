a80c052b56b7ae526cbcdfde5612c9ffbc61203f
Joyzyy <zavoianugabriel70@icloud.com>
2023-10-20 18:40:10 +0300

    Added dark/light theme functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import RootLayout from "@/layout/Root";
 import AppRoutes from "@/routes/AppRoutes";
+import { ThemeProvider } from "./components/ThemeProvider";
 
 const App: React.FC = () => {
   return (
-    <RootLayout>
-      <AppRoutes />
-    </RootLayout>
+    <ThemeProvider>
+      <RootLayout>
+        <AppRoutes />
+      </RootLayout>
+    </ThemeProvider>
   );
 };
 
